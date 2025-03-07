b5326cffc5cd7c699db51181314a44f22f0befc0
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-03-06 22:24:47 -0500

    feat(settings): Add settings route and profile picture ✨

--- app/routes/__appLayout.main_.settings/route.tsx
+++ app/routes/__appLayout.main_.settings/route.tsx
@@ -1,3 +1,61 @@
+import { MetaFunction } from "@remix-run/react";
+import { motion } from "motion/react";
+import { useUserData } from "~/contexts/user.context";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Zaimu - Settings" },
+    {
+      name: "description",
+      content:
+        "Zaimu is the easiest way to track your money, take control of your finances and save to achieve your dreams.",
+    },
+    { property: "og:url", content: "https://zaimu-finance.pages.dev/" },
+    { property: "og:type", content: "website" },
+    { property: "og:tittle", content: "Zaimu - Manage your Money" },
+    { property: "og:site_name", content: "Zaimu" },
+    {
+      property: "og:description",
+      content:
+        "Zaimu is the easiest way to track your money, take control of your finances and save to achieve your dreams.",
+    },
+    { name: "twitter:card", content: "summary_large_image" },
+    { property: "twitter:url", content: "https://zaimu-finance.pages.dev/" },
+    { name: "twitter:title", content: "Zaimu - Manage your Money" },
+    {
+      name: "twitter:description",
+      content:
+        "Zaimu is the easiest way to track your money, take control of your finances and save to achieve your dreams.",
+    },
+  ];
+};
+
 export default function Settings() {
-  return <section></section>;
+  const { user } = useUserData();
+
+  return (
+    <section className="grid gap-4">
+      <motion.span
+        className="font-bold text-neutral-700 text-2xl"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        Personal information
+      </motion.span>
+
+      <div className="flex gap-4">
+        <img
+          className="aspect-auto rounded-full"
+          width={300}
+          height={300}
+          src={user.profilePhoto?.url}
+          alt={`${user.name} ${user.lastName} profile`}
+        />
+
+        <div></div>
+      </div>
+    </section>
+  );
 }
